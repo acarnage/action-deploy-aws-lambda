e7a6c5f6265aaeb8c02385e6b742f1399509857c
Daniel <daniel@polombo.fr>
2020-09-22 13:14:54 +0200

    Include requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 deploy_function() {
 	echo "Deploying function ..."
 	cd "${INPUT_WORKING_DIRECTORY}"
+	add_requirements
 	zip -r code.zip . -x \*.git\*
 	aws lambda create-function --function-name "${INPUT_FUNCTION_NAME}" --runtime "${INPUT_RUNTIME}" --role "${INPUT_ROLE}" --handler "${INPUT_HANDLER}" --zip-file fileb://code.zip
 }
@@ -10,6 +11,7 @@ deploy_function() {
 update_function() {
 	echo "Updating function ..."
 	cd "${INPUT_WORKING_DIRECTORY}"
+	add_requirements
 	zip -r code.zip . -x \*.git\*
 	aws lambda update-function-configuration --function-name "${INPUT_FUNCTION_NAME}" --runtime "${INPUT_RUNTIME}" --role "${INPUT_ROLE}" --handler "${INPUT_HANDLER}"
 	aws lambda update-function-code --function-name "${INPUT_FUNCTION_NAME}" --zip-file fileb://code.zip
@@ -28,6 +30,16 @@ deploy_or_update_function() {
 		update_function
     fi
     echo "Done."
+}
+
+add_requirements() {
+	if [ -f "requirements.txt"]
+	then
+	    grep -v "^#" requirements.txt | while read pkg
+		do
+		    pip install -t ./ $pkg
+		done
+    fi
 }
 
 show_environment() {
